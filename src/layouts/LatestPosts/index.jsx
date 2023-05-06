536c1987600414ed3171b1a85ef28d73e15e9a8a
Christian Pascua <cristianpascuafdez@gmail.com>
2023-05-06 02:10:11 +0200

    Fixed key in map render latest posts

--- src/layouts/LatestPosts/index.jsx
+++ src/layouts/LatestPosts/index.jsx
@@ -15,8 +15,8 @@ const LatestPost = () => {
       <h2 className="text-primary text-5xl font-bold">Nuestro blog</h2>
       <h4 className="text-black text-2xl pt-4">Nuestros posts más recientes</h4>
       <div className="2xl:w-4/6 lg:w-5/6 w-full px-4 lg:px-0 mx-auto gap-8 pt-8 grid_latest_posts">
-        {latestPosts.map((post) => {
-          return <BlogCard post={post} />
+        {latestPosts.map((post, index) => {
+          return <BlogCard post={post} key={index}/>
         })}
       </div>
     </section>
